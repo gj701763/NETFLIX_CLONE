f36619253ab6e7c374dd02e9dddc5906129685cd
gj701763 <gj701763@gmail.com>
2024-08-14 14:31:44 +0530

    fist commit

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-// document.getElementById('menu-btn').addEventListener('click', function() {
-//     var mobileMenu = document.getElementById('mobile-menu');
-//     if (mobileMenu.classList.contains('hidden')) {
-//         mobileMenu.classList.remove('hidden');
-//     } else {
-//         mobileMenu.classList.add('hidden');
-//     }
-// });
 
-
-const dts = document.querySelectorAll("dt");
+const dts = document.querySelectorAll("dt"); 
 
     dts.forEach(dt => {
         dt.addEventListener("click", function() {
